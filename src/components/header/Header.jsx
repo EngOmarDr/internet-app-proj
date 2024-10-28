5db8f427635371349e6b817500e25a701c384568
hamza23177 <hamzamhamd23177@gmail.com>
2024-10-28 10:39:14 -0700

    Responsive with mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
-// Header.js
-
 import { Link } from 'react-router-dom'; // استيراد Link
 import './Header.css';
-import { FaBell, FaSearch } from 'react-icons/fa';
+import { FaBell, FaSearch, FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 import UserProfileMenu from '../user/UserProfileMenu';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // حالة القائمة
+
+  const toggleMenu = () => setMenuOpen(!menuOpen); // دالة لتبديل حالة القائمة
+
   return (
     <header className="header">
       <div className="header-logo">
@@ -23,13 +26,23 @@ const Header = () => {
           <FaSearch className="search-icon" />
           <input type="text" placeholder="Search..." className="search-input" />
         </div>
-        <Link to="/notifications" className="nav-link"> {/* أضف Link هنا */}
+        <Link to="/notifications" className="nav-link">
           <FaBell className="icon notification-icon" title="Notifications" />
         </Link>
         <UserProfileMenu/>
-        {/* <FaUserCircle className="icon profile-icon" title="Profile" /> */}
-        
+        <FaBars className="menu-toggle" onClick={toggleMenu} /> {/* زر القائمة للموبايل */}
       </div>
+
+      {/* قائمة منسدلة للروابط للموبايل */}
+      {menuOpen && (
+        <div className="mobile-menu show">
+          <Link to="/dashboard" className="nav-link-mobile" onClick={toggleMenu}>Dashboard</Link>
+          <Link to="/groups" className="nav-link-mobile" onClick={toggleMenu}>Groups</Link>
+          <Link to="/files" className="nav-link-mobile" onClick={toggleMenu}>Files</Link>
+          <Link to="/reports" className="nav-link-mobile" onClick={toggleMenu}>Reports</Link>
+          <Link to="/backup" className="nav-link-mobile" onClick={toggleMenu}>Backup & Restore</Link>
+        </div>
+      )}
     </header>
   );
 };
